7b8ef2beea8ff1f2ffba57ab5d9b0a22f691322b
MinhPhu <minhphuzcod@gmail.com>
2024-06-11 15:25:28 +0700

    Clock observer

--- Final_Project/src/final1/AnalogClock.java
+++ Final_Project/src/final1/AnalogClock.java
@@ -1,21 +1,44 @@
 package final1;
 
-public class AnalogClock implements Observer{
-	
-	
+public class AnalogClock implements Observer {
+
+	private int h, m, s;
+	private AnalogGUI analogGUI;
+	private ClockTime clockTime;
+
+	public AnalogClock(ClockTime clockTime) {
+		// TODO Auto-generated constructor stub
+		this.clockTime = clockTime;
+		clockTime.register(this);
+
+	}
 
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
-		
+		h = this.clockTime.getHour();
+		m = this.clockTime.getMinute();
+		s = this.clockTime.getSecond();
+		noti();
+
 	}
 
 	@Override
 	public void noti() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
+	public int getH() {
+		return h;
+	}
+
+	public int getM() {
+		return m;
+	}
+
+	public int getS() {
+		return s;
+	}
 
 }
